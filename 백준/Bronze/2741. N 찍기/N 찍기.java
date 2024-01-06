fa97cef349a1c87d74914fa524d6d4b5708b9a6c
HongJe <ajyong316@gmail.com>
2024-01-07 01:14:08 +0900

    [Bronze V] Title: N 찍기, Time: 1028 ms, Memory: 32636 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,15 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
 
-        int size = Integer.parseInt(br.readLine());
-        
+        int size = scanner.nextInt();
+
         for (int i = 1; i <= size; i++) {
             System.out.println(i);
         }
