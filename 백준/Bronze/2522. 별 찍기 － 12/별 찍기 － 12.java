4ab132d5e1fb0b92c0bfc4386e604222ee9dd81b
HongJe <ajyong316@gmail.com>
2024-01-11 02:28:45 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 204 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int size = sc.nextInt();
+
+        for (int i = 1; i <= size; i++) {
+            sb.append(" ".repeat(size - i));
+            sb.append("*".repeat(i));
+            sb.append("\n");
+        }
+
+        for (int j = 1; j <= size; j++) {
+            sb.append(" ".repeat(j));
+            sb.append("*".repeat(size - j));
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
