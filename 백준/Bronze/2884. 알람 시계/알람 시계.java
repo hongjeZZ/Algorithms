0669a92ac6a5ea78a876cea6cfad984b2b338724
HongJe <ajyong316@gmail.com>
2024-01-16 20:07:32 +0900

    [Bronze III] Title: 알람 시계, Time: 228 ms, Memory: 18512 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
-
+ 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int hour = sc.nextInt();
-        int min = sc.nextInt();
-
-        if (min >= 45) {
-            System.out.print(hour + " " + (min - 45));
-        } else {
-            if (hour != 0) {
-                System.out.print((hour - 1) + " " + (min + 15));
-            } else {
-                System.out.print(23 + " " + (min + 15));
-            }
-        }
-    }
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+		
+		int H = in.nextInt();
+		int M = in.nextInt();
+		in.close();
+		
+		if(M < 45) {
+			H--;
+			M= 60 - (45 - M);
+			if(H < 0) {
+				H = 23;
+			}
+			System.out.println(H + " " + M);
+		}
+		else {
+			System.out.println(H + " " + (M - 45));
+		}
+	}
 }
