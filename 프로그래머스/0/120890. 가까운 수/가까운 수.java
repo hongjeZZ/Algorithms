6c6cfdbb1f9490eaaaddaea8cb3b818c679308c6
HongJe <ajyong316@gmail.com>
2024-07-03 02:21:44 +0900

    [level 0] Title: 가까운 수, Time: 0.04 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        int minDiff = Integer.MAX_VALUE;
+            
+        for (int num : array) {
+            int diff = Math.abs(num - n);
+            
+            if (diff < minDiff) {
+                minDiff = diff;
+                answer = num;
+            } else if (diff == minDiff && num < answer) {
+                answer = num;
+            }
+        } 
+        
+        return answer;
+    }
+}
