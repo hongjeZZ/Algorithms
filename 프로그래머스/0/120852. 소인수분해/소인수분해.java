a8febf831078dc101e863d8e93096ddbe3f3a9a6
HongJe <ajyong316@gmail.com>
2024-07-03 04:41:47 +0900

    [level 0] Title: 소인수분해, Time: 2.49 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -1,18 +1,18 @@
 import java.util.LinkedHashSet;
+import java.util.Set;
 
 class Solution {
     public int[] solution(int n) {
-        LinkedHashSet<Integer> primeNumbers = new LinkedHashSet<>();
+        Set<Integer> set = new LinkedHashSet<>();
 
         int i = 2;
-        while (i <= n){
-            if(n % i==0){
-                primeNumbers.add(i);
+        while (i <= n) {
+            if (n % i == 0) {
+                set.add(i);
                 n /= i;
-             } else{
-                i++;
-            }
+            } else i ++;
         }
-        return primeNumbers.stream().mapToInt(Integer::intValue).toArray();
+
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
