3782f2a3f65b5d604552760159694d2b9ae15ba2
HongJe <ajyong316@gmail.com>
2024-07-08 00:34:51 +0900

    [level 1] Title: 실패율, Time: 3.33 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=java) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 3.86 ms
+메모리: 77.4 MB, 시간: 3.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 07일 23:05:42
+2024년 07월 08일 00:34:48
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -5,27 +5,25 @@ class Solution {
     public int[] solution(int N, int[] stages) {
         int[] users = new int[N + 2];
 
-        Map<Integer, Double> map = new HashMap<>();
-
-        for (int i = 0; i < stages.length; i++) {
-            users[stages[i]]++;
+        for (int stage : stages) {
+            users[stage]++;
         }
 
-        int numOfUser = stages.length;
+        Map<Integer, Double> map = new HashMap<>();
+        double numOfUser = stages.length;
 
         for (int i = 1; i <= N; i++) {
             if (users[i] == 0) {
-                map.put(i, 0.0);
+                map.put(i, 0.);
             }
             else {
-                map.put(i, (double) users[i] / numOfUser);
+                map.put(i, users[i] / numOfUser);
                 numOfUser -= users[i];
             }
         }
-
-        return map.entrySet().stream().sorted(
-                (o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
-                .mapToInt(Map.Entry::getKey)
-                .toArray();
+        
+        return map.entrySet().stream()
+                .sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
+                .mapToInt(Map.Entry::getKey).toArray();
     }
 }
