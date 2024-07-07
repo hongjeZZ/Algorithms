ac2415350d65fe96ec44400e8c423a263efb11e1
HongJe <ajyong316@gmail.com>
2024-07-08 03:06:10 +0900

    [level 2] Title: 방문 길이, Time: 17.00 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -5,21 +5,20 @@ import java.util.Set;
 
 class Solution {
     public static final Map<Character, int[]> location = new HashMap<>();
-    
+
     void initLocation() {
-        location.put('U', new int[] { 0, 1 });
-        location.put('D', new int[] { 0, -1 });
-        location.put('R', new int[] { 1, 0 });
-        location.put('L', new int[] { -1, 0 });
+        location.put('U', new int[]{0, 1});
+        location.put('D', new int[]{0, -1});
+        location.put('R', new int[]{1, 0});
+        location.put('L', new int[]{-1, 0});
     }
 
     boolean isMovable(int x, int y) {
         return x >= 0 && x < 11 && y >= 0 && y < 11;
     }
-    
+
     public int solution(String dirs) {
         initLocation();
-
         Set<String> answer = new HashSet<>();
 
         int x = 5;
@@ -27,18 +26,15 @@ class Solution {
 
         for (char c : dirs.toCharArray()) {
             int[] offset = location.get(c);
-            int nx = x + offset[0];
-            int ny = y + offset[1];
 
-            if (!isMovable(nx, ny)) {
+            if (!isMovable(x + offset[0], y + offset[1])) {
                 continue;
             }
 
-            answer.add(x + " " + y + " " + nx + " " + ny);
-            answer.add(nx + " " + ny + " " + x + " " + y);
-
-            x = nx;
-            y = ny;
+            answer.add(x + " " + y + " " + (x + offset[0]) + " " + (y + offset[1]));
+            answer.add((x + offset[0]) + " " + (y + offset[1]) + " " + x + " " + y);
+            x += offset[0];
+            y += offset[1];
         }
 
         return answer.size() / 2;
