78dcd079c488154a423601c7915e8bc7383ade28
HongJe <ajyong316@gmail.com>
2024-07-13 23:24:10 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.15 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -3,32 +3,23 @@ import java.util.Stack;
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
-        Stack<Integer>[] lanes = new Stack[board.length];
-        Stack<Integer> bracket = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < lanes.length; i++) {
-            lanes[i] = new Stack<>();
-        }
-
-        for (int i = board.length - 1; i >= 0; i--) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] != 0) {
-                    lanes[j].push(board[i][j]);
+        for (int move : moves) {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][move-1] != 0) {
+                    if (stack.isEmpty() || stack.peek() != board[i][move-1]) {
+                        stack.push(board[i][move-1]);
+                    } else {
+                        stack.pop();
+                        answer += 2;
+                    }
+                    board[i][move-1] = 0;
+                    break;
                 }
             }
         }
 
-        for (int move : moves) {
-            if (lanes[move-1].isEmpty()) continue;
-
-            if (bracket.isEmpty() || lanes[move-1].peek() != bracket.peek()) {
-                bracket.push(lanes[move-1].pop());
-            } else {
-                lanes[move-1].pop();
-                bracket.pop();
-                answer += 2;
-            }
-        }
         return answer;
     }
 }
