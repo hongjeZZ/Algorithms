4b93bcf902d7e6642fbf60c6ecd49c366bcd934b
HongJe <ajyong316@gmail.com>
2024-07-13 18:40:17 +0900

    [level 2] Title: 짝지어 제거하기, Time: 59.84 ms, Memory: 63.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (Character c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else {
+                if (stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+
+        return stack.isEmpty()? 1 : 0;
+    }
+}
