d19d8991d327d23bb34be992a20aaa219dc24739
HongJe <ajyong316@gmail.com>
2024-10-25 18:38:42 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -6,32 +6,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int res = 0;
+
         String input = br.readLine();
-        int minusIdx = input.indexOf('-');
+        String[] split = input.split("-");
 
-        int sum = 0;
-        
-        // 마이너스 부호가 없다면 모든 수를 더함
-        if (minusIdx == -1) {
-            String[] numbers = input.split("\\+");
-            for (String numStr : numbers) {
-                sum += Integer.parseInt(numStr);
+        for (int i = 0; i < split.length; i++) {
+            int sum = 0;
+
+            String[] nums = split[i].split("\\+");
+
+            for (String num : nums) {
+                sum += Integer.parseInt(num);
             }
-            System.out.println(sum);
-            return;
+
+            if (i == 0) {
+                res += sum;
+            } else {
+                res -= sum;
+            }
         }
 
-        // 마이너스 부호가 있다면 부호 앞 문자를 모두 더하고, 부호 뒤 문자를 모두 뺌
-        String[] plusNumbers = input.substring(0, minusIdx).split("\\+");
-        for (String plusNumber : plusNumbers) {
-            sum += Integer.parseInt(plusNumber);
-        }
-
-        String[] minusNumbers = input.substring(minusIdx).replaceAll("[^0-9]", " ").trim().split("\\s+");
-        for (String minusNumber : minusNumbers) {
-            sum -= Integer.parseInt(minusNumber);
-        }
-        
-        System.out.println(sum);
+        System.out.println(res);
     }
 }
