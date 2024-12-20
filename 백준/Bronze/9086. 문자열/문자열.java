c8b681dec4c78d045094d605dca5de44abdf52ea
안홍제 <ajyong316@gmail.com>
2024-12-20 18:25:08 +0900

    [Bronze V] Title: 문자열, Time: 100 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            String input = br.readLine();
+            sb.append(input.charAt(0));
+            sb.append(input.charAt(input.length() - 1));
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
