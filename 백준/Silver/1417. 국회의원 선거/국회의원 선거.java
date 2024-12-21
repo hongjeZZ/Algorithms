da9895047466720ce7dd8482abde76c9479e14a5
안홍제 <ajyong316@gmail.com>
2024-12-22 01:18:37 +0900

    [Silver V] Title: 국회의원 선거, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,34 +18,18 @@ public class Main {
             return;
         }
 
-        int[] arr = new int[N - 1];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int answer = 0;
 
-        // 1번을 제외한 각 후보의 득표수 저장
+        // 1번을 제외한 각 후보의 득표수를 우선순위 큐에 저장
         for (int i = 0; i < N - 1; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            pq.add(Integer.parseInt(br.readLine()));
         }
 
-        while (true) {
-            // 각 후보들의 최대값과 해당 인덱스 저장
-            int idx = -1;
-            int max = Integer.MIN_VALUE;
-
-            // 최대값 찾기
-            for (int i = 0; i < N - 1; i++) {
-                if (arr[i] > max) {
-                    max = arr[i];
-                    idx = i;
-                }
-            }
-            if (target > max) {
-                break;
-            }
-            
-            // 1번 득표수를 하나 올리고, 최대 득표자의 득표수를 하나 내림
+        while (!pq.isEmpty() && pq.peek() >= target) {
             target++;
-            arr[idx]--;
             answer++;
+            pq.add(pq.poll() - 1);
         }
         System.out.println(answer);
     }
