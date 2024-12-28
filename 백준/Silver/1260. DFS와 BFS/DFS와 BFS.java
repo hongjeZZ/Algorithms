13bd2f29ef93927fc36ceca84031c1c8943a5f4a
안홍제 <ajyong316@gmail.com>
2024-12-28 23:27:03 +0900

    [Silver II] Title: DFS와 BFS, Time: 260 ms, Memory: 21276 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,79 +2,85 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N;   // 정점의 갯수
+    static int M;   // 간선의 갯수
+    static int Y;   // 시작할 정점의 번호
+    static boolean[] visited;   // 방문 여부
+    static List<List<Integer>> graph = new ArrayList<>();   // BFS 에서 쓰일 Queue
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] NMY = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 정점의 갯수
-        int N = Integer.parseInt(NMY[0]);
-        // 간선의 갯수
-        int M = Integer.parseInt(NMY[1]);
-        // 시작할 정점의 번호
-        int Y = Integer.parseInt(NMY[2]);
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        Y = Integer.parseInt(st.nextToken());
 
         // 노드 정보 저장
-        List<List<Integer>> graph = new ArrayList<>();
-
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < M; i++) {
-            String[] AB = br.readLine().split(" ");
-            int A = Integer.parseInt(AB[0]);
-            int B = Integer.parseInt(AB[1]);
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
             graph.get(A).add(B);
             graph.get(B).add(A);
         }
 
         // 그래프의 각 인접 리스트를 오름차순 정렬
         for (int i = 1; i <= N; i++) {
-            graph.get(i).sort(Integer::compareTo);
+            Collections.sort(graph.get(i));
         }
 
-        boolean[] visited = new boolean[N + 1];
-        dfs(graph, Y, visited);
-        System.out.println();
+        visited = new boolean[N + 1];
+        dfs(Y);
+        sb.append("\n");
 
         visited = new boolean[N + 1];
-        bfs(graph, Y, visited);
+        bfs(Y);
+        System.out.println(sb);
     }
 
     // DFS 알고리즘
-    public static void dfs(List<List<Integer>> graph, int Y, boolean[] visited) {
+    public static void dfs(int idx) {
         // 시작 정점 방문 처리
-        visited[Y] = true;
-        System.out.print(Y + " ");
+        visited[idx] = true;
+        sb.append(idx + " ");
 
         // 정점과 연결된 노드 순회
-        for (int i = 0; i < graph.get(Y).size(); i++) {
-            Integer y = graph.get(Y).get(i);
+        for (int i = 0; i < graph.get(idx).size(); i++) {
+            Integer y = graph.get(idx).get(i);
 
             // 방문하지 않은 노드가 있다면 재귀함수 호출
             if (!visited[y]) {
-                dfs(graph, y, visited);
+                dfs(y);
             }
         }
     }
 
     // BFS 알고리즘
-    public static void bfs(List<List<Integer>> graph, int Y, boolean[] visited) {
+    public static void bfs(int idx) {
         Queue<Integer> queue = new LinkedList<>();
         // 시작 정점 방문 처리
-        visited[Y] = true;
-        queue.offer(Y);
+        visited[idx] = true;
+        queue.offer(idx);
 
         // 큐가 비어있지 않을 때까지 반복
         while (!queue.isEmpty()) {
             // 큐에 저장된 하나의 원소를 뽑아 출력
             Integer x = queue.poll();
-            System.out.print(x + " ");
+            sb.append(x + " ");
 
             // 그 원소에 인접한 노드 중 방문하지 않는 원소들을 큐에 삽입
             for (int i = 0; i < graph.get(x).size(); i++) {
