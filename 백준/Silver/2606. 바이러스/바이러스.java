8f99c5afd26cee628bfbe183701775d9629871b6
안홍제 <ajyong316@gmail.com>
2024-12-30 10:27:15 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,16 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int count = -1;
     static List<List<Integer>> list = new ArrayList<>();
-    static Queue<Integer> q = new LinkedList<>();
     static boolean[] infected;
 
     public static void main(String[] args) throws IOException {
@@ -34,26 +31,18 @@ public class Main {
         }
 
         infected = new boolean[N + 1];
-        bfs(1);
+        dfs(1);
         System.out.println(count);
     }
 
-    static void bfs(int idx) {
-        q.offer(idx);
+    static void dfs(int idx) {
         infected[idx] = true;
         count++;
 
-        while (!q.isEmpty()) {
-            Integer x = q.poll();
-
-            for (int i = 0; i < list.get(x).size(); i++) {
-                Integer y = list.get(x).get(i);
-
-                if (!infected[y]) {
-                    q.offer(y);
-                    infected[y] = true;
-                    count++;
-                }
+        for (int i = 0; i < list.get(idx).size(); i++) {
+            Integer y = list.get(idx).get(i);
+            if (!infected[y]) {
+                dfs(y);
             }
         }
     }
