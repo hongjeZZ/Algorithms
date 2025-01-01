797fbc6014c2380c7e3e97f54ad1ad10ed83884a
안홍제 <ajyong316@gmail.com>
2025-01-01 21:09:53 +0900

    [Silver IV] Title: 수 찾기, Time: 504 ms, Memory: 48156 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Set<Integer> set = new LinkedHashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+
+            if (set.contains(target)) {
+                bw.write(1 + "\n");
+            } else {
+                bw.write(0 + "\n");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
