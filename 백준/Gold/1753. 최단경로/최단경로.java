dd23becbc911fae22741710283c37682fc43ad74
안홍제 <ajyong316@gmail.com>
2025-01-06 21:01:33 +0900

    [Gold IV] Title: 최단경로, Time: 668 ms, Memory: 107888 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,13 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static List<Node>[] list;
+    static List<List<Node>> list = new ArrayList<>();
 
     static class Node implements Comparable<Node> {
         int idx;
@@ -33,14 +34,9 @@ public class Main {
         int E = Integer.parseInt(st.nextToken());
         int start = Integer.parseInt(br.readLine());
 
-        list = new List[V + 1];
-        int[] distance = new int[V + 1];
-        boolean[] visited = new boolean[V + 1];
-
         // List 초기화
-        for (int i = 1; i < V + 1; i++) {
-            list[i] = new ArrayList<>();
-            distance[i] = Integer.MAX_VALUE;
+        for (int i = 0; i < V + 1; i++) {
+            list.add(new ArrayList<>());
         }
 
         // 노드, 간선, 비용 저장
@@ -51,14 +47,17 @@ public class Main {
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            list[u].add(new Node(v, w));
+            list.get(u).add(new Node(v, w));
         }
+
+        // 최단 거리 저장 배열
+        int[] distance = new int[V + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
 
         PriorityQueue<Node> pq = new PriorityQueue<>();
 
         // start 노드는 비용이 0
         pq.offer(new Node(start, 0));
-        visited[start] = true;
         distance[start] = 0;
 
         while (!pq.isEmpty()) {
@@ -66,19 +65,21 @@ public class Main {
             Node poll = pq.poll();
             int idx = poll.idx;
             int cost = poll.cost;
-            
-            if (!visited[idx]) {
-                visited[idx] = true;
+
+            // 이미 방문한 노드라면 즉시 종료
+            if (cost > distance[idx]) {
+                continue;
             }
 
-            for (Node next : list[idx]) {
+            // 인접한 노드의 거리 계산
+            for (int i = 0; i < list.get(idx).size(); i++) {
                 // 새로운 비용
-                int newCost = cost + next.cost;
+                int newCost = cost + list.get(idx).get(i).cost;
 
                 // 새로운 비용이 더 저렴하다면 교체 및 우선순위 큐에 담기
-                if (!visited[next.idx] && distance[next.idx] > newCost) {
-                    distance[next.idx] = newCost;
-                    pq.offer(new Node(next.idx, newCost));
+                if (newCost < distance[list.get(idx).get(i).idx]) {
+                    distance[list.get(idx).get(i).idx] = newCost;
+                    pq.offer(new Node(list.get(idx).get(i).idx, newCost));
                 }
             }
         }
