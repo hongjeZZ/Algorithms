9d2713aa167d324c9416f61fd1a5bc7a5c91f446
안홍제 <ajyong316@gmail.com>
2025-01-10 09:33:53 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -8,16 +8,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
         String[] strings = new String[]{"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-        int answer = 0;
 
         for (String str : strings) {
             while (input.contains(str)) {
                 input = input.replaceFirst(str, " ");
-                answer++;
             }
         }
-        
-        input = input.replaceAll(" ", "");
-        System.out.println(answer + input.length());
+        System.out.println(input.length());
     }
 }
