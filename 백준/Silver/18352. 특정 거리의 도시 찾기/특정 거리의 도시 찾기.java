a258e9daa6f585745ee2c59e06e20cb7806213d2
안홍제 <ajyong316@gmail.com>
2025-01-16 09:42:20 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1108 ms, Memory: 284708 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -43,10 +42,10 @@ public class Main {
 
         list = new ArrayList[N + 1];
         distance = new int[N + 1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
 
         for (int i = 1; i < N + 1; i++) {
             list[i] = new ArrayList<>();
+            distance[i] = -1;
         }
 
         for (int i = 0; i < M; i++) {
@@ -57,16 +56,16 @@ public class Main {
         }
 
         dijkstra(X);
-        int cnt = 0;
 
+        boolean check = false;
         for (int i = 1; i < N + 1; i++) {
             if (distance[i] == K) {
-                cnt++;
+                check = true;
                 sb.append(i).append("\n");
             }
         }
 
-        System.out.println(cnt == 0 ? -1 : sb);
+        System.out.println(check ? sb : -1);
     }
 
     public static void dijkstra(int start) {
@@ -87,7 +86,7 @@ public class Main {
                 int nextIdx = next.idx;
                 int nextCost = next.cost + cost;
 
-                if (distance[nextIdx] > nextCost) {
+                if (distance[nextIdx] == -1 || distance[nextIdx] > nextCost) {
                     distance[nextIdx] = nextCost;
                     pq.offer(new Node(nextIdx, nextCost));
                 }
