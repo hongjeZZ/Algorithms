d8faffa5f8bfa1bd2cfae66bf0764c5770e09370
안홍제 <ajyong316@gmail.com>
2025-01-16 09:47:44 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1024 ms, Memory: 277700 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -2,7 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,23 +13,8 @@ public class Main {
     static int K;
     static int X;
 
-    static ArrayList<Node>[] list;
+    static ArrayList<Integer>[] list;
     static int[] distance;
-
-    static class Node implements Comparable<Node> {
-        int idx;
-        int cost;
-
-        public Node(int idx, int cost) {
-            this.idx = idx;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return cost - o.cost;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -52,10 +38,25 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            list[from].add(new Node(to, 1));
+            list[from].add(to);
         }
 
-        dijkstra(X);
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(X);
+        distance[X] = 0;
+
+        while (!q.isEmpty()) {
+            int idx = q.poll();
+
+            for (int next : list[idx]) {
+                if (distance[next] == -1) {
+                    // 최단 거리 갱신
+                    distance[next] = distance[idx] + 1;
+                    q.offer(next);
+                }
+            }
+        }
+
 
         boolean check = false;
         for (int i = 1; i < N + 1; i++) {
@@ -66,31 +67,5 @@ public class Main {
         }
 
         System.out.println(check ? sb : -1);
-    }
-
-    public static void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
-        distance[start] = 0;
-
-        while (!pq.isEmpty()) {
-            Node poll = pq.poll();
-            int idx = poll.idx;
-            int cost = poll.cost;
-
-            if (distance[idx] < cost) {
-                continue;
-            }
-
-            for (Node next : list[idx]) {
-                int nextIdx = next.idx;
-                int nextCost = next.cost + cost;
-
-                if (distance[nextIdx] == -1 || distance[nextIdx] > nextCost) {
-                    distance[nextIdx] = nextCost;
-                    pq.offer(new Node(nextIdx, nextCost));
-                }
-            }
-        }
     }
 }
