94e6ef4f4b59a8f51dff21114abda9dfddbe78e6
안홍제 <ajyong316@gmail.com>
2025-01-17 21:44:10 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        char[][] arr = new char[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+
+            for (int j = 0; j < line.length(); j++) {
+                arr[i][j] = line.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i] != '\u0000') {
+                    sb.append(arr[j][i]);
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
