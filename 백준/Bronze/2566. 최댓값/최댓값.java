0cfe9b4462191e8ee74a55e0abb70de86c946b9d
안홍제 <ajyong316@gmail.com>
2025-01-17 21:36:48 +0900

    [Bronze III] Title: 최댓값, Time: 128 ms, Memory: 15948 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int max = Integer.MIN_VALUE;
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                int value = Integer.parseInt(st.nextToken());
+
+                if (max < value) {
+                    max = value;
+                    x = i + 1;
+                    y = j + 1;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(x + " " + y);
+    }
+}
