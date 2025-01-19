74ee004105ed31b974159c719569fffb1a4b8438
안홍제 <ajyong316@gmail.com>
2025-01-19 22:37:56 +0900

    [Silver V] Title: 단어 정렬, Time: 312 ms, Memory: 24828 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,17 +10,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        List<String> list = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            String value = br.readLine();
-
-            if (!list.contains(value)) {
-                list.add(value);
-            }
-        }
-
-        list.sort((o1, o2) -> {
+        Set<String> set = new TreeSet<>((o1, o2) -> {
             if (o1.length() != o2.length()) {
                 return Integer.compare(o1.length(), o2.length());
             } else {
@@ -28,7 +18,11 @@ public class Main {
             }
         });
 
-        for (String val : list) {
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        for (String val : set) {
             sb.append(val).append("\n");
         }
         System.out.println(sb);
