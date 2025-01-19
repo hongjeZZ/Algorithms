34c6c520a95493fb74b5b31a6b2016a3f7b42ce2
안홍제 <ajyong316@gmail.com>
2025-01-19 22:21:12 +0900

    [Silver V] Title: 영화감독 숌, Time: 268 ms, Memory: 85468 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int value = 666;
+        int cnt = 1;
+
+        while (cnt != N) {
+            value++;
+            String string = String.valueOf(value);
+
+            if (string.contains("666")) {
+                cnt++;
+            }
+        }
+
+        System.out.println(value);
+    }
+}
