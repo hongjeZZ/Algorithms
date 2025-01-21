b2b892c09609a41b351c7d8eee292a06f3b7f0bf
안홍제 <ajyong316@gmail.com>
2025-01-21 10:04:55 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 320 ms, Memory: 25928 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -6,29 +6,22 @@ import java.util.PriorityQueue;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        // 낮은 숫자가 우선 순위인 Integer 우선순위 큐
-        PriorityQueue<Integer> cards = new PriorityQueue<>();
-
-        // 카드 추가
         for (int i = 0; i < N; i++) {
-            cards.add(Integer.parseInt(br.readLine()));
+            pq.offer(Integer.parseInt(br.readLine()));
         }
 
-        int res = 0;
+        int sum = 0;
 
-        // 카드가 하나 남을 때까지 반복
-        while (cards.size() > 1) {
-            // 가장 작은 카드 뽑기
-            int newCard = cards.poll() + cards.poll();
-            res += newCard;
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
 
-            // 새로운 카드를 카드 뭉치에 추가
-            cards.add(newCard);
+            sum += a + b;
+            pq.offer(a + b);
         }
-
-        System.out.println(res);
+        System.out.println(sum);
     }
 }
