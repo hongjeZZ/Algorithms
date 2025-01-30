bd2cc10ea2c451ec6fe76a8426d3a006787505ff
안홍제 <ajyong316@gmail.com>
2025-01-30 15:15:39 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -8,14 +8,12 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         long[] dp = new long[101];
+        dp[0] = 0;
         dp[1] = 1;
         dp[2] = 1;
-        dp[3] = 1;
-        dp[4] = 2;
-        dp[5] = 2;
 
-        for (int i = 6; i < 101; i++) {
-            dp[i] = dp[i - 1] + dp[i - 5];
+        for (int i = 3; i < 101; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
         }
 
         int T = Integer.parseInt(br.readLine());
