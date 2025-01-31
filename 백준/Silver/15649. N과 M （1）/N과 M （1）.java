f112ab6fab04a0282a607db6f8c20d2f27e16c2f
안홍제 <ajyong316@gmail.com>
2025-01-31 16:07:35 +0900

    [Silver III] Title: N과 M (1), Time: 220 ms, Memory: 23232 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -7,6 +7,7 @@ public class Main {
     static int N;
     static int M;
     static boolean[] visited;
+    static int[] selected;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,13 +16,15 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
+        selected = new int[M];
         visited = new boolean[N + 1];
 
-        combination(new ArrayList<>(), 0);
+        combination(0);
         System.out.println(sb);
     }
 
-    public static void combination(List<Integer> selected, int depth) {
+    public static void combination(int depth) {
         if (depth == M) {
             for (int value : selected) {
                 sb.append(value).append(" ");
@@ -33,10 +36,9 @@ public class Main {
         for (int i = 1; i < N + 1; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                selected.add(i);
-                combination(selected, depth + 1);
+                selected[depth] = i;
+                combination(depth + 1);
                 visited[i] = false;
-                selected.remove(selected.size() - 1);
             }
         }
     }
