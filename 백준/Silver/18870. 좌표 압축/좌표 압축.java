09d7e198e84c054f4a8b455753b61ef16a3ef885
안홍제 <ajyong316@gmail.com>
2025-02-04 15:11:27 +0900

    [Silver II] Title: 좌표 압축, Time: 1852 ms, Memory: 265800 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] clone = arr.clone();
+        Arrays.sort(clone);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int rank = 0;
+        
+        for (int i = 0; i < N; i++) {
+            if (!map.containsKey(clone[i])) {
+                map.put(clone[i], rank);
+                rank++;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            sb.append(map.get(arr[i])).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
