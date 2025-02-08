84c5154be418e5418b05477307a19e1271d4816e
안홍제 <ajyong316@gmail.com>
2025-02-08 15:26:35 +0900

    [Gold V] Title: 빗물, Time: 116 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -11,52 +11,29 @@ public class Main {
         int H = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[H][W];
-
+        int[] height = new int[W];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < W; i++) {
-            int wall = Integer.parseInt(st.nextToken());
-
-            for (int j = 0; j < wall; j++) {
-                arr[H - 1 - j][i] = 1;
-            }
+            height[i] = Integer.parseInt(st.nextToken());
         }
 
         int cnt = 0;
 
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                // 벽이라면 종료
-                if (arr[i][j] == 1) {
-                    continue;
-                }
+        // 가장 자리에는 빗물이 고일 수 없다.
+        for (int i = 1; i < W - 1; i++) {
+            int left = 0;
+            int right = 0;
 
-                // 벽이 아니라면, 같은 높이의 벽이 주변에 있는 지 확인
-                // 좌측 벽 확인
-                int left = j - 1;
-                boolean leftFlag = false;
+            for (int j = 0; j < i; j++) {
+                left = Math.max(left, height[j]);
+            }
 
-                for (int k = left; k >= 0; k--) {
-                    if (arr[i][k] == 1) {
-                        leftFlag = true;
-                        break;
-                    }
-                }
+            for (int j = i + 1; j < W; j++) {
+                right = Math.max(right, height[j]);
+            }
 
-                // 우측 벽 확인
-                int right = j + 1;
-                boolean rightFlag = false;
-
-                for (int k = right; k < W; k++) {
-                    if (arr[i][k] == 1) {
-                        rightFlag = true;
-                        break;
-                    }
-                }
-
-                if (leftFlag && rightFlag) {
-                    cnt++;
-                }
+            if (Math.min(left, right) > height[i]) {
+                cnt += Math.min(left, right) - height[i];
             }
         }
         System.out.println(cnt);
