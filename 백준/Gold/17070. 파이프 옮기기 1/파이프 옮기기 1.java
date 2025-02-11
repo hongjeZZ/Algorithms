2e38322d5335ab2f9b373bbc13a586f701a7d32d
안홍제 <ajyong316@gmail.com>
2025-02-11 11:20:33 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -4,16 +4,11 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N;
-    static int[][] arr;
-    static int cnt = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -22,31 +17,33 @@ public class Main {
             }
         }
 
-        dfs(0, 1, 0);
-        System.out.println(cnt);
-    }
+        int[][][] dp = new int[N][N][3];
+        dp[0][1][0] = 1;
 
-    public static void dfs(int x, int y, int direction) {
-        if (x == N - 1 && y == N - 1) {
-            cnt++;
-            return;
-        }
+        for (int x = 0; x < N; x++) {
+            for (int y = 1; y < N; y++) {
+                if (arr[x][y] == 1) {
+                    continue;
+                }
 
-        // 우측으로 이동이 가능하다면
-        if (direction != 1 && canMove(x, y + 1)) {
-            dfs(x, y + 1, 0);
-        }
-        // 하단으로 이동이 가능하다면
-        if (direction != 0 && canMove(x + 1, y)) {
-            dfs(x + 1, y, 1);
-        }
-        // 우측 대각으로 이동이 가능하다면
-        if (canMove(x, y + 1) && canMove(x + 1, y) && canMove(x + 1, y + 1)) {
-            dfs(x + 1, y + 1, 2);
-        }
-    }
+                // 1. 가로 방향 (좌측에서 오는 경우)
+                dp[x][y][0] += dp[x][y - 1][0] + dp[x][y - 1][2];
 
-    public static boolean canMove(int x, int y) {
-        return x < N && y < N && arr[x][y] != 1;
+                // 2. 세로 방향 (위쪽에서 오는 경우)
+                if (x - 1 >= 0) {
+                    dp[x][y][1] += dp[x - 1][y][1] + dp[x - 1][y][2];
+                }
+
+                // 3. 대각선 방향 (대각선, 가로, 세로 모두 확인 필요)
+                if (x - 1 >= 0 && arr[x - 1][y] == 0 && arr[x][y - 1] == 0) {
+                    dp[x][y][2] += dp[x - 1][y - 1][0] + dp[x - 1][y - 1][1] + dp[x - 1][y - 1][2];
+                }
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += dp[N - 1][N - 1][i];
+        }
+        System.out.println(sum);
     }
 }
