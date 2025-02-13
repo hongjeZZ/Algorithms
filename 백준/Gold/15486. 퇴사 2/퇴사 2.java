1e49996c4e64c0b96289f9ce9980ef95c3d89846
안홍제 <ajyong316@gmail.com>
2025-02-13 15:11:25 +0900

    [Gold V] Title: 퇴사 2, Time: 672 ms, Memory: 310164 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -16,7 +16,7 @@ public class Main {
             value[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[N + 50];
+        int[] dp = new int[N + 2];
 
         // 1일부터 N일까지 반복
         for (int i = 1; i < N + 1; i++) {
@@ -24,7 +24,9 @@ public class Main {
             dp[i + 1] = Math.max(dp[i], dp[i + 1]);
 
             // 선택
-            dp[i + time[i]] = Math.max(dp[i] + value[i], dp[i + time[i]]);
+            if (i + time[i] < N + 2) {
+                dp[i + time[i]] = Math.max(dp[i] + value[i], dp[i + time[i]]);
+            }
         }
 
         System.out.println(dp[N + 1]);
