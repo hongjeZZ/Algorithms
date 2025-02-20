c6b5f1f8858983c56636b77864845fc7381459b5
안홍제 <ajyong316@gmail.com>
2025-02-20 14:07:59 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 112 ms, Memory: 15380 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -1,24 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static class Node {
-        int[] SCV;
-        int cnt;
-
-        public Node(int[] SCV, int cnt) {
-            this.SCV = SCV.clone();
-            this.cnt = cnt;
-        }
-    }
-
+    static int[][][] dp = new int[61][61][61];
     static int[] attack = {9, 3, 1};
-    static boolean[][][] visited = new boolean[61][61][61];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,45 +18,40 @@ public class Main {
             SCV[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(bfs(SCV));
+        // DP 테이블 초기화
+        for (int[][] arr2 : dp) {
+            for (int[] arr : arr2) {
+                Arrays.fill(arr, -1);
+            }
+        }
+        System.out.println(dfs(SCV[0], SCV[1], SCV[2]));
     }
 
-    public static int bfs(int[] SCV) {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(SCV, 0));
-        visited[SCV[0]][SCV[1]][SCV[2]] = true;
+    public static int dfs(int a, int b, int c) {
+        if (a <= 0 && b <= 0 && c <= 0) return 0;
+        
+        a = Math.max(a, 0);
+        b = Math.max(b, 0);
+        c = Math.max(c, 0);
 
-        while (!q.isEmpty()) {
-            Node node = q.poll();
-            int[] scv = node.SCV;
-            int cnt = node.cnt;
+        // 이미 방문한 경우
+        if (dp[a][b][c] != -1) return dp[a][b][c];
 
-            // 모든 SCV 체력이 0 이하인 경우 최소 공격 횟수 반환
-            if (scv[0] <= 0 && scv[1] <= 0 && scv[2] <= 0) {
-                return cnt;
-            }
+        int minValue = Integer.MAX_VALUE;
 
-            // 6가지 공격 패턴 수행
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (i == j) continue;
-                    for (int k = 0; k < 3; k++) {
-                        if (i == k || j == k) continue;
-
-                        int[] nextSCV = {
-                                Math.max(scv[0] - attack[i], 0),
-                                Math.max(scv[1] - attack[j], 0),
-                                Math.max(scv[2] - attack[k], 0)
-                        };
-
-                        if (!visited[nextSCV[0]][nextSCV[1]][nextSCV[2]]) {
-                            visited[nextSCV[0]][nextSCV[1]][nextSCV[2]] = true;
-                            q.offer(new Node(nextSCV, cnt + 1));
-                        }
-                    }
+        // 6가지 공격 조합 수행
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j) continue;
+                for (int k = 0; k < 3; k++) {
+                    if (i == k || j == k) continue;
+                    
+                    int dfs = dfs(a - attack[i], b - attack[j], c - attack[k]);
+                    minValue = Math.min(minValue, dfs);
                 }
             }
         }
-        return -1;
+        // 최소 공격 횟수 반환
+        return dp[a][b][c] = minValue + 1;
     }
 }
