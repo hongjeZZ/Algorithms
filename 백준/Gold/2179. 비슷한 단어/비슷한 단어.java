340da12aa5f51a2e0b62bb52b0d5a350316446ad
안홍제 <ajyong316@gmail.com>
2025-02-21 14:50:39 +0900

    [Gold IV] Title: 비슷한 단어, Time: 1524 ms, Memory: 19232 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -2,20 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        List<String> list = new ArrayList<>();
+        Set<String> uniqueStrings = new LinkedHashSet<>();
         for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            if (!list.contains(line)) {
-                list.add(line);
-            }
+            uniqueStrings.add(br.readLine());
         }
+
+        List<String> list = new ArrayList<>(uniqueStrings);
+
 
         int max = 0;
         String A = "";
