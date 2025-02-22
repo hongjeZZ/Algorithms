cf18baae9d8e1a3d3f3b9a856690e126da0399ec
안홍제 <ajyong316@gmail.com>
2025-02-22 23:01:35 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,31 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] NK = br.readLine().split(" ");
-        int N = Integer.parseInt(NK[0]);
-        int K = Integer.parseInt(NK[1]);
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        // 코인 정렬 (내림차순)
         int[] coins = new int[N];
-        for (int i = N - 1; i >= 0; i--) {
+        for (int i = 0; i < N; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
 
-        // 최소 코인 계산
         int cnt = 0;
-
-        for (int i = 0; i < N; i++) {
-            if (K >= coins[i]) {
-                int coinCount = K / coins[i];
-                K -= coins[i] * coinCount;
-                cnt += coinCount;
+        for (int i = N - 1; i >= 0 && K > 0; i--) {  // O(N)
+            if (coins[i] <= K) {
+                cnt += K / coins[i];
+                K %= coins[i];
             }
         }
+
         System.out.println(cnt);
     }
 }
