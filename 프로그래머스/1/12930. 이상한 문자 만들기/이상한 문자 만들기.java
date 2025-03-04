483100ac958701c18471609c1e378559160463f3
안홍제 <ajyong316@gmail.com>
2025-03-04 19:57:06 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.15 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                sb.append(c);
+                index = 0;
+            } else {
+                sb.append(index % 2 == 0 ? Character.toUpperCase(c) : Character.toLowerCase(c));
+                index++;
+            }
+        }
+
+        return sb.toString();
+    }
+}
