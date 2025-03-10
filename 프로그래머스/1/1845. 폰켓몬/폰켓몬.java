9e235405a68a76fc242e3efa9759d962b84d9514
안홍제 <ajyong316@gmail.com>
2025-03-10 20:26:31 +0900

    [level 1] Title: 폰켓몬, Time: 4.82 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int N = nums.length / 2;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        return Math.min(N, set.size());
+    }
+}
