8fca78f71d3ff105276241ba54341e9127217ecf
안홍제 <ajyong316@gmail.com>
2025-03-10 20:36:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.39 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,19 +4,18 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
         
-        for (String person : participant) {
-            map.put(person, map.getOrDefault(person, 0) + 1);
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
         
-        for (String person : completion) {
-            map.replace(person, map.get(person) - 1);
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
         }
         
-        for (String person : map.keySet()) {
-            if (map.get(person) > 0) {
-                return person;
-            }
-        }
-        return "";
+        return map.entrySet().stream()
+                .filter(e -> e.getValue() > 0)
+                .findFirst()
+                .get()
+                .getKey();
     }
 }
