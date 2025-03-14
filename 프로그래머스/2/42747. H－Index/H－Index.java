030afb43f38e0844beacff1905997be2f7cfc295
안홍제 <ajyong316@gmail.com>
2025-03-14 18:04:42 +0900

    [level 2] Title: H-Index, Time: 1.00 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+
+        int length = citations.length;
+
+        for (int i = 0; i < length; i++) {
+            int h = length - i;
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+}
