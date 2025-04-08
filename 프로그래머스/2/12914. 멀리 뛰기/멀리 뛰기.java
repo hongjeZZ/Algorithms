94c9442bc4fd9f3d8a1a2892d35c92b75ac61a79
안홍제 <ajyong316@gmail.com>
2025-04-08 22:33:31 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,14 +1,13 @@
 class Solution {
     public long solution(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-
+        int MOD = 1234567;
         long[] dp = new long[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
         }
 
         return dp[n];
