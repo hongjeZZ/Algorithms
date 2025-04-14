574a0960a049a8436c871d89d2dc50bc31d9f77f
안홍제 <ajyong316@gmail.com>
2025-04-14 22:21:54 +0900

    [level 2] Title: 피보나치 수, Time: 4.57 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -5,9 +5,9 @@ class Solution {
         dp[1] = 1;
         
         for (int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1_234_567;
         }
         
-        return (int) dp[n] % 1234567;
+        return (int) dp[n] % 1_234_567;
     }
 }
